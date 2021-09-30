9e633b503b622cfd56d7cf89f6901f820057ad3c
min2ha <minvidm@gmail.com>
2021-09-30 11:40:41 +0100

    Initial update - add search method

--- api/api.py
+++ api/api.py
@@ -14,5 +14,16 @@ def read_root():
 def read_category(category_id: int, q: Optional[str] = None):
     return {"category_id": category_id, "q": q}
 
+
+@app.route('/search', methods=['GET', 'POST'])
+# @login_required
+def search():
+    # form = SearchForm()
+    # if request.method == 'POST' and form.validate_on_submit():
+    #    return redirect((url_for('search_results', query=form.search.data)))  # or what you want
+    # return render_template('search.html', form=form)
+    return {"Search result": "List of Categories & Sub-Categories"}
+
+
 if __name__ == '__main__':
     uvicorn.run(api, port=8000, host="0.0.0.0")
