9d904b2a5a496873d618d48926d3eb7f17197c5e
Mindaugas Vidmantas <mindaugasvidmantas@MindaugassMBP5.broadband>
2021-10-03 14:21:32 +0100

    Initial commit update - pysolr added

--- api/api.py
+++ api/api.py
@@ -1,8 +1,29 @@
-from typing import Optional
+from typing import List, Optional
+from pydantic import BaseModel
 from fastapi import FastAPI
+import pysolr
 import uvicorn
 
 api = FastAPI()
+
+class Category(BaseModel):
+
+    collectionAreaId: int
+    name = 'Collection no X'
+    description = 'Collection no X'
+    id : int
+    type : str
+
+class Collection(BaseModel):
+    id : int
+    type : str
+    name : str
+    description : str
+    collectionAreaId : int # covers and Categories - matters query grouping ONLY 
+
+class CollectionList(BaseModel):
+    __root__: List[Collection]
+
 
 
 @api.get("/")
@@ -23,6 +44,27 @@ def search():
     #    return redirect((url_for('search_results', query=form.search.data)))  # or what you want
     # return render_template('search.html', form=form)
     return {"Search result": "List of Categories & Sub-Categories"}
+
+
+
+conn = pysolr.Solr('http://192.168.1.160:8984/solr/collections', search_handler='/select', use_qt_param=False)
+result = conn.search('type:collection AND collectionAreaId:[* TO *]', **{
+    'indent': 'true',
+    'rows':'1000',
+    'group.field':'collectionAreaId',
+    'group.limit':'1000',
+    'group':'true'
+})
+
+top_category_list = []
+
+for r in result.raw_response['grouped']['collectionAreaId']['groups']:
+    top_category_list.append(r['groupValue'])
+
+@api.get("/categories/")
+async def read_categories():
+    top_categories = {"categories": top_category_list}
+    return top_categories
 
 
 if __name__ == '__main__':
