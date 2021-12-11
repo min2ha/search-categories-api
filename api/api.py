89057b071a7bdce1a329aeebad1b8f236481397b
min2ha <minvidm@gmail.com>
2021-12-11 22:58:16 +0000

    initial version for pivot

--- api/api.py
+++ api/api.py
@@ -43,26 +43,68 @@ def search():
     # if request.method == 'POST' and form.validate_on_submit():
     #    return redirect((url_for('search_results', query=form.search.data)))  # or what you want
     # return render_template('search.html', form=form)
+    print('Please wait while the program is loading...')
     return {"Search result": "List of Categories & Sub-Categories"}
 
 
 
-conn = pysolr.Solr('http://192.168.1.160:8984/solr/collections', search_handler='/select', use_qt_param=False)
-result = conn.search('type:collection AND collectionAreaId:[* TO *]', **{
+conn = pysolr.Solr('http://192.168.1.160:38983/solr/collections', search_handler='/select', use_qt_param=False)
+result = conn.search('*:*', **{
+    
+     # fl=collectionAreaId&
+     # fq=collectionAreaId:[*%20TO%20*]-collectionAreaId:(2945)&
+     # indent=on&
+     # facet=true&
+     # facet.limit=-1&
+     # facet.pivot.mincount=1&
+     # facet.pivot=collectionAreaId,id&
+     # q=*:*&
+     # wt=json
+    
+    
+
+    'fl':'collectionAreaId',
+    'fq':'collectionAreaId:[* TO *]-collectionAreaId:(2945)',
+
+    'facet':'true',
+    'facet.limit':'-1',
+    'facet.pivot.mincount':'1',
+    'facet.pivot':'collectionAreaId,id,description,name',
+
     'indent': 'true',
-    'rows':'1000',
-    'group.field':'collectionAreaId',
-    'group.limit':'1000',
-    'group':'true'
+
+    'wt':'json'
+
+
 })
+
+print('SOLR Pivot data for Categories...')
+
+# {'field': 'description', 'value': 'This collection was initiated in 2016 and ...
+print(result.raw_response['facet_counts']['facet_pivot']['collectionAreaId,id,description,name'][1]['pivot'][0]['pivot'])
+
+# JSON array - brackets
+# 'pivot': [{'field': 'name', 'value': 'Online Enthusiast Communities in the UK'
+# [{'field': 'name', 'value': 'Online Enthusiast Communities in the UK', 'count': 1}]
+print(result.raw_response['facet_counts']['facet_pivot']['collectionAreaId,id,description,name'][1]['pivot'][0]['pivot'][0]['pivot'])
+
+# not JSON array !!!!!!!! - no brackets
+# {'field': 'name', 'value': 'Online Enthusiast Communities in the UK', 'count': 1}
+print(result.raw_response['facet_counts']['facet_pivot']['collectionAreaId,id,description,name'][1]['pivot'][0]['pivot'][0]['pivot'][0])
+
+# not JSON array ! - specific JSON  field value !!!!!!!!
+# Online Enthusiast Communities in the UK
+print(result.raw_response['facet_counts']['facet_pivot']['collectionAreaId,id,description,name'][1]['pivot'][0]['pivot'][0]['pivot'][0]['value'])
+
 
 top_category_list = []
 
-for r in result.raw_response['grouped']['collectionAreaId']['groups']:
-    top_category_list.append(r['groupValue'])
+#for r in result.raw_response['grouped']['collectionAreaId']['groups']:
+#    top_category_list.append(r['groupValue'])
 
 @api.get("/categories/")
 async def read_categories():
+    print('Please wait while the program is loading...')
     top_categories = {"categories": top_category_list}
     return top_categories
 
